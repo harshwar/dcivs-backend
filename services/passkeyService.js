39f6baaa527d28b5e72f72691308d026322e70eb
harshwar <harsh15022003@gmail.com>
2026-02-27 15:59:03 +0530

    fix(passkey): add production domains to allowlist

--- services/passkeyService.js
+++ services/passkeyService.js
@@ -20,6 +20,8 @@ const ALLOWED_ORIGINS = [
   'http://localhost:5175',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
+  'https://dcivs.online',
+  'https://www.dcivs.online',
   process.env.RP_ORIGIN
 ].filter(Boolean);
 
